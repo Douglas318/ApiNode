2b709a564af9e00f0b8e682df405917fa1094856
Douglas Freitas <freitas28douglas@gmail.com>
2022-06-01 15:57:31 -0300

    Update tarefa.js

--- ApiNode/tarefa.js
+++ ApiNode/tarefa.js
@@ -1,22 +1,18 @@
 const api = require("./poke")
-
 const express = require('express')
- 
 const app = express();
 
 app.use(express.json());
-
 
 app.get('/', (req, res) =>{
     return res.send({ mensagem: "Douglas"});
 })
 
-
 app.get('/pokemon/:id', async (req, res) => {
     const {id} = req.params;
     try {
         const {data} = await api.get(`pokemon/${id}`)
-
+        
         return res.send({nome: data.name});
     } catch (error) {
         res.send({error: error.message})
