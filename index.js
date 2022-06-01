8da3865d30d271718393f7991f3866ce39897128
Douglas Freitas <freitas28douglas@gmail.com>
2022-06-01 15:56:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express')
-
 const app = express()
-
 app.use(express.json())
 
-app.post("/array", (req, res) => {
-    
+app.post("/array", (req, res) => {    
     const {SomaArray} = req.body;
-
+    
     res.send(SomaArray.join(" "))
 
 })
